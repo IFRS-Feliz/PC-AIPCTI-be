feb068c372bab7a3464f91f470b6773ba58bb6c0
Rafael Scholz Griebler <rafaelscholz03@gmail.com>
2021-04-08 18:24:28 -0300

    Sistema de autenticacao refeito e varios endpoints adicionados

--- app.js
+++ app.js
@@ -5,6 +5,32 @@ const app = express();
 const port = 5000;
 
 const mysql = require("mysql");
+const session = require("express-session");
+// const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["POST", "GET", "PUT"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(
+  session({
+    key: "userId",
+    secret: "dijfoisdjfasf01923fi203fj92u3f09",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 60 * 60 * 24 * 365,
+    },
+  })
+);
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -18,9 +44,6 @@ connection.connect((err) => {
   }
 });
 
-app.use(cors());
-app.use(express.json());
-
 app.post("/login", (req, res) => {
   connection.query(
     "SELECT * FROM usuario WHERE email=? AND senha=?",
@@ -30,14 +53,165 @@ app.post("/login", (req, res) => {
         throw error;
       }
       if (results.length > 0) {
-        console.log("existe");
-        res.json(results);
+        req.session.user = results[0];
+        res.json({
+          isAuthenticated: true,
+          email: req.session.user.email,
+          isAdmin: req.session.user.isAdmin,
+        });
       } else {
-        console.log("nao existe");
-        res.json({ message: "Credentials incorrect" });
+        res.status(401).json({ isAuthenticated: false });
       }
     }
   );
+});
+
+app.get("/usuario", (req, res) => {
+  connection.query("SELECT * FROM usuario", (error, results, fields) => {
+    if (error) {
+      throw error;
+    }
+    if (req.session.user) {
+      if (req.session.user.isAdmin === 1) {
+        res.json({
+          isAuthenticated: true,
+          email: req.session.user.email,
+          isAdmin: req.session.user.isAdmin,
+          results,
+        });
+      } else {
+        res.status(403).json({
+          isAuthenticated: true,
+          email: req.session.user.email,
+          isAdmin: false,
+        });
+      }
+    } else {
+      res.status(401).json({ isAuthenticated: false });
+    }
+  });
+});
+
+app.get("/projeto", (req, res) => {
+  if (req.session.user) {
+    if (req.session.user.isAdmin === 1) {
+      if (req.query.cpfUsuario) {
+        connection.query(
+          "SELECT * FROM projeto WHERE cpfUsuario=?",
+          [req.query.cpfUsuario],
+          (error, results, fields) => {
+            if (error) {
+              throw error;
+            }
+            res.json({
+              isAuthenticated: true,
+              email: req.session.user.email,
+              isAdmin: req.session.user.isAdmin,
+              results,
+            });
+          }
+        );
+      }
+    } else {
+      res.status(403).json({
+        isAuthenticated: true,
+        email: req.session.user.email,
+        isAdmin: false,
+      });
+    }
+  } else {
+    res.status(401).json({ isAuthenticated: false });
+  }
+});
+
+app.put("/projeto", (req, res) => {
+  if (req.session.user) {
+    if (req.session.user.isAdmin === 1) {
+      if (req.body.cpfUsuario && req.body.id) {
+        connection.query(
+          "UPDATE projeto SET nome=?, valorRecebidoTotal=?, valorRecebidoCapital=?, valorRecebidoCusteio=?, idEdital=? WHERE cpfUsuario=? AND id=?",
+          [
+            req.body.projetoNewInfo.nome,
+            req.body.projetoNewInfo.valorRecebidoTotal,
+            req.body.projetoNewInfo.valorRecebidoCapital,
+            req.body.projetoNewInfo.valorRecebidoCusteio,
+            req.body.projetoNewInfo.idEdital,
+            req.body.cpfUsuario,
+            req.body.id,
+          ],
+          (error, results, fields) => {
+            if (error) {
+              throw error;
+            }
+            res.status(200).send();
+          }
+        );
+      }
+    } else {
+      res.status(403).json({
+        isAuthenticated: true,
+        email: req.session.user.email,
+        isAdmin: false,
+      });
+    }
+  } else {
+    res.status(401).json({ isAuthenticated: false });
+  }
+});
+
+app.get("/edital", (req, res) => {
+  connection.query("SELECT * FROM edital", (error, results, fields) => {
+    if (error) {
+      throw error;
+    }
+    if (req.session.user) {
+      if (req.session.user.isAdmin === 1) {
+        res.json({
+          isAuthenticated: true,
+          email: req.session.user.email,
+          isAdmin: req.session.user.isAdmin,
+          results,
+        });
+      } else {
+        res.status(403).json({
+          isAuthenticated: true,
+          email: req.session.user.email,
+          isAdmin: false,
+        });
+      }
+    } else {
+      res.status(401).json({ isAuthenticated: false });
+    }
+  });
+});
+
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.clearCookie("userId", { path: "/" });
+  res.status(200).send();
+});
+
+//403=logado mas nao admin
+//401=nao logado
+
+app.get("/login", (req, res) => {
+  if (req.session.user) {
+    if (req.session.user.isAdmin === 1) {
+      res.json({
+        isAuthenticated: true,
+        email: req.session.user.email,
+        isAdmin: req.session.user.isAdmin,
+      });
+    } else {
+      res.status(403).json({
+        isAuthenticated: true,
+        email: req.session.user.email,
+        isAdmin: false,
+      });
+    }
+  } else {
+    res.status(401).json({ isAuthenticated: false });
+  }
 });
 
 app.listen(port, () => {
