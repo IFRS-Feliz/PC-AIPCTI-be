2ce27027746416ddf8a5b69781281b822d8470e1
Rafael Scholz Griebler <rafaelscholz03@gmail.com>
2021-04-22 13:31:46 -0300

    Adicionados e editados metodos para edicao e usuarios

--- routes/projeto.js
+++ routes/projeto.js
@@ -33,7 +33,12 @@ router
   })
   .post(
     body("cpfUsuario").isLength({ min: 11, max: 11 }),
-    body("projetos").exists(),
+    body("projetos").isArray(),
+    body("projetos.*.nome").exists(),
+    body("projetos.*.valorRecebidoTotal").isNumeric(),
+    body("projetos.*.valorRecebidoCusteio").isNumeric(),
+    body("projetos.*.valorRecebidoCapital").isNumeric(),
+    body("projetos.*.idEdital").isNumeric(),
     (req, res) => {
       if (!validationResult(req).isEmpty()) {
         return res.sendStatus(400);
@@ -45,21 +50,10 @@ router
       let query =
         "INSERT INTO projeto (cpfUsuario, nome, valorRecebidoTotal, valorRecebidoCapital, valorRecebidoCusteio, idEdital) VALUES";
 
-      let error = false;
       projetos.forEach((projeto) => {
-        if (
-          !projeto.nome ||
-          isNaN(projeto.valorRecebidoTotal) ||
-          isNaN(projeto.valorRecebidoCapital) ||
-          isNaN(projeto.valorRecebidoCusteio) ||
-          isNaN(projeto.idEdital)
-        ) {
-          error = true;
-          return res.sendStatus(400);
-        }
         query += `('${cpfUsuario}', '${projeto.nome}', ${projeto.valorRecebidoTotal}, ${projeto.valorRecebidoCapital}, ${projeto.valorRecebidoCusteio}, ${projeto.idEdital}),`;
       });
-      if (error) return;
+
       query = query.substring(0, query.length - 1) + ";";
 
       connection.query(
@@ -76,52 +70,59 @@ router
     }
   )
   .put(
-    body("cpfUsuario").isLength({ min: 11, max: 11 }),
-    body("id").isNumeric(),
-    body("projetoNewInfo.nome").exists(),
-    body("projetoNewInfo.valorRecebidoTotal").isNumeric(),
-    body("projetoNewInfo.valorRecebidoCusteio").isNumeric(),
-    body("projetoNewInfo.valorRecebidoCapital").isNumeric(),
+    //.*. quer dizer em qualquer posicao da lista projetos
+    body("projetos").isArray().isLength({ min: 1 }),
+    body("projetos.*.id").isNumeric(),
+    body("projetos.*.nome").exists(),
+    body("projetos.*.valorRecebidoTotal").isNumeric(),
+    body("projetos.*.valorRecebidoCusteio").isNumeric(),
+    body("projetos.*.valorRecebidoCapital").isNumeric(),
+    body("projetos.*.idEdital").isNumeric(),
     (req, res) => {
       if (!validationResult(req).isEmpty()) {
         return res.sendStatus(400);
       }
 
-      connection.query(
-        "UPDATE projeto SET nome=?, valorRecebidoTotal=?, valorRecebidoCapital=?, valorRecebidoCusteio=?, idEdital=? WHERE cpfUsuario=? AND id=?",
-        [
-          req.body.projetoNewInfo.nome,
-          req.body.projetoNewInfo.valorRecebidoTotal,
-          req.body.projetoNewInfo.valorRecebidoCapital,
-          req.body.projetoNewInfo.valorRecebidoCusteio,
-          req.body.projetoNewInfo.idEdital,
-          req.body.cpfUsuario,
-          req.body.id,
-        ],
-        (error, results, fields) => {
-          if (error) {
-            return sendStatus(500);
-          }
+      let query =
+        "INSERT INTO projeto (id, nome, valorRecebidoTotal, valorRecebidoCapital, valorRecebidoCusteio, idEdital) VALUES";
 
-          res.status(200).json({ user: req.user, token: req.token });
+      req.body.projetos.forEach((projeto) => {
+        query += `(${projeto.id}, "${projeto.nome}", ${projeto.valorRecebidoTotal}, ${projeto.valorRecebidoCapital}, ${projeto.valorRecebidoCusteio}, ${projeto.idEdital}),`;
+      });
+      query = query.substring(0, query.length - 1);
+
+      query +=
+        " ON DUPLICATE KEY UPDATE nome=VALUES(nome), valorRecebidoTotal=VALUES(valorRecebidoTotal), valorRecebidoCapital=VALUES(valorRecebidoCapital), valorRecebidoCusteio=VALUES(valorRecebidoCusteio), idEdital=VALUES(idEdital);";
+
+      connection.query(query, (error, results, fields) => {
+        if (error) {
+          return res.sendStatus(500);
         }
-      );
+
+        res.status(200).json({ user: req.user, token: req.token });
+      });
     }
   )
-  .delete(body("id").isNumeric(), (req, res) => {
-    if (!validationResult(req).isEmpty()) {
-      return res.sendStatus(400);
-    }
+  .delete(
+    body("projetos").isArray(),
+    body("projetos.*.id").isNumeric(),
+    (req, res) => {
+      if (!validationResult(req).isEmpty()) {
+        return res.sendStatus(400);
+      }
 
-    connection.query(
-      "DELETE FROM projeto WHERE id = ?",
-      [req.body.id],
-      (error) => {
+      let query = "DELETE FROM projeto WHERE id IN (";
+      req.body.projetos.forEach((projeto) => {
+        query += `${projeto.id},`;
+      });
+      query = query.substring(0, query.length - 1) + ");";
+
+      connection.query(query, (error) => {
         if (error) return res.sendStatus(500);
 
         res.status(200).json({ token: req.token, user: req.user });
-      }
-    );
-  });
+      });
+    }
+  );
 
 module.exports = router;
